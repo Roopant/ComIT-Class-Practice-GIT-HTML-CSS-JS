0cb2cb530deaa204300d43086405510bec0056ff
roopantkalra <roopantkalra@gmail.com>
2022-07-04 23:32:30 -0600

    started javascript demo and variable declaration

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Declaring variables*/
+let age=6;
+console.log(age)
+console.log('age:',age)
+const address= '211 fallswater road'
+console.log('address:', address)
+address= '45 calgary'
+console.log('address:', address)
