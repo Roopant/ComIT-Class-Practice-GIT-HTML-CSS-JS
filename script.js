47114827170dcd8dc5784fccb06da31b580c25b9
roopantkalra <roopantkalra@gmail.com>
2022-07-05 00:37:16 -0600

    primitive data types

--- script.js
+++ script.js
@@ -4,5 +4,33 @@ console.log(age)
 console.log('age:',age)
 const address= '211 fallswater road'
 console.log('address:', address)
-address= '45 calgary'
+
 console.log('address:', address)
+
+/*data types*/
+
+/*string*/
+const name ='roopant'
+console.log ('name :',name)
+const a=''
+console.log(typeof a)
+
+/*numbers-integers or decimals(floats)*/
+const height =5
+console.log('height:',height)
+console.log(typeof height)
+
+/*boolean-true or false*/
+const isCanadian=false
+console.log('Roopant is Canadian:',isCanadian)
+console.log(typeof isCanadian)
+
+/*Undefined=we dont know*/
+const disease= undefined
+console.log(disease)
+console.log(typeof disease)
+
+/*Null=Non-existence of anything*/
+const myFather=null
+console.log(myFather)
+console.log(typeof myFather)
