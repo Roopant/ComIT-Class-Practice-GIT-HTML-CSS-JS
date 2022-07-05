9f6a0a90329506f92c200d53f11563c5074397e7
roopantkalra <roopantkalra@gmail.com>
2022-07-05 02:07:29 -0600

    non-primitive data-types

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 
 
-/*Primitive data types*/
+/*Non-Primitive data type- OBJECT/
 
-/*string*/
-const name ='roopant'
-console.log ('name :',name)
-const a=''
-console.log(typeof a)
+/*Array-subcategory of object*/
 
-/*numbers-integers or decimals(floats)*/
-const height =5
-console.log('height:',height)
-console.log(typeof height)
+const kids=['Nikunj','Prisha','Nayra']
+console.log(typeof kids)
+console.log(kids)
+console.log(kids[1])
+console.log(kids[4])//undefined-javascript dont have any information about fourth element as it doesn't exist ,so it is default behaviour
 
-/*boolean-true or false*/
-const isCanadian=false
-console.log('Roopant is Canadian:',isCanadian)
-console.log(typeof isCanadian)
+/*Array with mix data type*/
+const nephew=['Nikunj',10,true]
+console.log(nephew)
+console.log (nephew[2]) //access true value
 
-/*Undefined=we dont know*/
-const disease= undefined
-console.log(disease)
-console.log(typeof disease)
+/*array within array*/
+const niece=['Prisha',14,['girl',true]]
+console.log(niece)
 
-/*Null=Non-existence of anything*/
-const myFather=null
-console.log(myFather)
-console.log(typeof myFather)
+console.log(niece[2][0])// access girl value
+
+/* to reassign values*/
+ niece[1]=15
+ console.log[niece]
+console.log(typeof niece)
+
+niece[1]=16
+console.log[niece]
+
+
+/*Object-subcategory of object*/
+const person={
+    name:'Roopant',
+    age:29,
+    isCanadian:false,
+}
+console.log('person:',person)
+console.log(typeof person)
+console.log(person.age)//roopant age by dot notation
+console.log(person['name'])//Roopant name by square notation
+person.name='Roopant kalra' //to reassign value
+console.log(person)
+
+niece[0]='Parisha'
+console.log(niece)
